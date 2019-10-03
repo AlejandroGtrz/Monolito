9d81627899d4fd6c29e59b8e98486d7f09c359d1
uo251694 <uo251694@gmail.com>
2019-10-03 12:19:53 +0000

    sad

--- api.py
+++ api.py
@@ -183,5 +183,4 @@ def login():
 if __name__=='__main__':
     if not os.path.exists('db.sqlite'):
         db.create_all()
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
